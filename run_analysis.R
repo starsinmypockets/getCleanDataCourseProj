aa01e782404f9590cf16c7df05e7f03999164a9d
Paul Walker <pjwalker76@gmail.com>
2014-12-21 17:25:07 -0500

    Add codebook

--- run_analysis.R
+++ run_analysis.R
@@ -5,10 +5,7 @@
 ##
 
 # Helper functions
-# @argument list - a numeric list of activities to be re-labeled
-# @returns list - list with string labels for activities
-##
-addActivityNames <-function (name) {
+mapActivityNames <-function (name) {
     switch(name,
         '1' = 'WALKING',
         '2' = 'WALKING_UPSTAIRS',
@@ -20,11 +17,12 @@ addActivityNames <-function (name) {
 }
 
 ##
-# 1. Merge training and test sets to create one data set
+# (1) Merge training and test sets to create one data set
+# (4) Appropriately labels the data set with descriptive variable names
 ##
-
 wd <- dirname(sys.frame(1)$ofile)  # script directory
 features <- read.table(paste(wd,'/data/features.txt',sep=""))  #variable names
+features <- sapply(features, function (x) {gsub('[()-,]', "", x); }); #remove weird characters from colnames
 
 #test data
 xtest <- read.table(paste(wd,'/data/test/X_test.txt',sep=""), col.names=features[,2]);
@@ -42,33 +40,30 @@ colnames(test2)[1] <- 'activity'; # fix activity col name
 xtrain <- read.table(paste(wd,'/data/train/X_train.txt',sep=""), col.names=features[,2]);
 trainSubjects <- read.table(paste(wd,'/data/train/subject_train.txt',sep=""), col.names=c("subject"));
 trainActivity <- read.table(paste(wd,'/data/train/y_train.txt',sep=""), col.names=c("activity"));
+
 trainActivity <- apply(trainActivity, 1, addActivityNames);
 
 train1 <- cbind(trainSubjects, xtrain); # add subject col
 train2 <- cbind(trainActivity, train1); # add activity col
 colnames(train2)[1] <- 'activity'; # fix activity col name
 
-# complete raw dataset
-complete <- merge(test2, train2, );
+# entire raw dataset
+complete <- rbind(test2, train2);
 
-##
-# 2. Extract only the measurements on the mean and standard deviation for each measurement
-##
+# (2) Extract only the measurements on the mean and standard deviation for each measurement
+fields1 <- c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240:241, 253:254, 266:271, 294:296, 345:350, 373:375, 424:429, 452:454, 503:504, 513, 516:517, 526, 529, 539, 542:543, 552, 555:561)
+fields2 <- c(1,2, sapply(fields1, function (x) {x+2})); # re-index fields to account for subject & activity fields which come first
 
-# fields we want, indexed to features.txt
-c(1:6,41:46,81:86,121:126,161:166,201,202,214,215,227,228,240,241,253,254,266:271,345:350,424:429,503,504,516,517,529,530,542,543)# add subject and activity cols and add 2 to the fields index to account for our added columns
-fields <- c(1,2, sapply(fields, function (x) {x+2}));
-
-# subset of complete data 
-tidySet <- complete[,fields]
-
-##
-# 3.Use descriptive activity names to name the activities in the data set
-##
-
-##
-# 4. Appropriately labels the data set with descriptive variable names
-##
+# subset of raw dataset 
+tidySet <- complete[,fields2];
+byActivity <- aggregate(tidySet[,3:87], list(Activity=tidySet$activity), mean); # show avgs by activity
+bySubject <- aggregate(tidySet[,3:87], list(Subject=tidySet$subject), mean); # show avgs by subject
+byActivityBySubject <- aggregate(tidySet[,3:87], tidySet[,1:2], mean); # show avgs by subject by activity
 
 # Creates a second, independent tidy data set with the average 
 #     of each variable for each activity and each subject
+
+# avg by activity
+    
+# avg by subject
+
